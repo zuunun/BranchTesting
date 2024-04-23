d21233ba8ef427253b3778837744092f996df93b
zuunun <zuhurunun@MacBook-Air-tilhrende-Zuhur.local>
2024-04-23 14:27:35 +0200

    name commit

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,6 @@
 public abstract class Animal {
     private String name;
+
+    public abstract void makeSound();
+
 }
---
+++ src/Cat.java
@@ -0,0 +1,9 @@
+public class Cat extends Animal{
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+
+
+}
